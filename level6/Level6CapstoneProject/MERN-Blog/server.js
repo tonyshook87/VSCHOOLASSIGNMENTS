ed6288ea828f717b019b1302d65526110031ad5d
Anthony Shook <tonyshook87@gmail.com>
2025-03-27 19:01:37 -0500

    updated deployment settings

--- level6/Level6CapstoneProject/MERN-Blog/server.js
+++ level6/Level6CapstoneProject/MERN-Blog/server.js
@@ -18,7 +18,12 @@ const connectDB = async () => {
   }
 };
 connectDB();
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  });
+}
 // Middleware
 
 // Routes
